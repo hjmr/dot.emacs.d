0d0523a72dc4c6653a27bbbe3e3173c10eebbe2e
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2018-11-04 23:28:33 +0900

    Fixed for Windows 10

--- init.el
+++ init.el
@@ -412,43 +412,40 @@ properly disable mozc-mode."
 ;;  macOS
 ;;-----------------------------
 ;;
-
 (my-def-font "hirakaku" "Monaco" my-ascii-font-size 'medium "Hiragino Kaku Gothic Pro" my-jp-font-size 'medium)
 (my-def-font "hiramin" "Monaco" my-ascii-font-size 'medium "Hiragino Mincho Pro" my-jp-font-size 'medium)
 (my-def-font "hirasans" "Inconsolata" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light)
 (my-def-font "lettergoth" "Letter Gothic Std" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light)
 (my-def-font "inputmono" "Input Mono Narrow" my-ascii-font-size 'light "Hiragino Sans" my-jp-font-size 'light)
 (my-def-font "udkyokasho" "Input Mono Narrow" my-ascii-font-size 'light "UD Digi Kyokasho N-R" my-jp-font-size 'medium)
-
+;;
 ;;-----------------------------
 ;;  Linux
 ;;-----------------------------
-
 (my-def-font "taakoexgoth" "Ubuntu Mono" my-ascii-font-size 'light "TakaoExゴシック" my-jp-font-size 'medium)
 (my-def-font "notosans" "DejaVu Sans Mono" my-ascii-font-size 'light "Noto Sans CJK JP" my-jp-font-size 'light)
 (my-def-font "vlgoth" "DejaVu Sans Mono" my-ascii-font-size 'light "VL Gothic" my-jp-font-size 'light)
-
+;;
 ;;-----------------------------
 ;;  Windows
 ;;-----------------------------
 ;;
-
 (my-def-font "meiryo" "Consolas" my-ascii-font-size 'medium "メイリオ" my-jp-font-size 'medium)
 (my-def-font "udkyokashowin" "Consolas" my-ascii-font-size 'medium "UD デジタル 教科書体 N-R" my-jp-font-size 'medium)
-
+;;
 ;;-----------------------------
 ;;  General
 ;;-----------------------------
-
+;;
 (my-def-font "ricty" "Ricty Diminished" my-ascii-font-size 'medium "Ricty Diminished" my-jp-font-size 'medium)
-
+;;
 ;;-------- Default Font ----------
 (when gui-mac-or-ns-p
   (set-default-font "fontset-udkyokasho")
   (add-to-list 'default-frame-alist '(font . "fontset-udkyokasho")))
 (when gui-win-p
-  (set-default-font "fontset-udkyokasho")
-  (add-to-list 'default-frame-alist '(font . "fontset-udkyokasho")))
+  (set-default-font "fontset-udkyokashowin")
+  (add-to-list 'default-frame-alist '(font . "fontset-udkyokashowin")))
 (when gui-x-p
   (when sys-centos-p
     (set-default-font "fontset-vlgoth")
