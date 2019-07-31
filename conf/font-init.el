818b33027924b67c64fc4c575895ab44b94f9167
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2019-07-31 11:15:39 +0900

    Disable font anti-aliases.

--- conf/font-init.el
+++ conf/font-init.el
@@ -49,10 +49,10 @@
     (t          14)))
 (defvar my-jp-font-size (truncate (* my-ascii-font-size 1.2)))
 ;;
-(defun my-def-font (name asciifont asciifont-size asciifont-weight jpfont jpfont-size jpfont-weight)
+(defun my-def-font (name asciifont asciifont-size asciifont-weight jpfont jpfont-size jpfont-weight anti-alias)
   (ignore-errors
-    (let* ((fontspec (font-spec :family asciifont :size asciifont-size :weight asciifont-weight))
-           (jp-fontspec (font-spec :family jpfont :size jpfont-size :weight jpfont-weight))
+    (let* ((fontspec (font-spec :family asciifont :size asciifont-size :weight asciifont-weight :antialias anti-alias))
+           (jp-fontspec (font-spec :family jpfont :size jpfont-size :weight jpfont-weight :antialias anti-alias))
            (fsn (create-fontset-from-ascii-font asciifont nil name)))
       (set-fontset-font fsn 'ascii fontspec)
       (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
@@ -72,32 +72,32 @@
   (setq fixed-width-use-QuickDraw-for-ascii t)
   (setq mac-allow-anti-aliasing nil))
 ;;
-(my-def-font "hirakaku" "Monaco" my-ascii-font-size 'medium "Hiragino Kaku Gothic Pro" my-jp-font-size 'medium)
-(my-def-font "hiramin" "Monaco" my-ascii-font-size 'medium "Hiragino Mincho Pro" my-jp-font-size 'medium)
-(my-def-font "hirasans" "Inconsolata" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light)
-(my-def-font "lettergoth" "Letter Gothic Std" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light)
-(my-def-font "inputmono" "Input Mono Narrow" my-ascii-font-size 'light "Hiragino Sans" my-jp-font-size 'light)
-(my-def-font "udkyokasho" "Input Mono Narrow" my-ascii-font-size 'light "UD Digi Kyokasho N-R" my-jp-font-size 'medium)
+(my-def-font "hirakaku" "Monaco" my-ascii-font-size 'medium "Hiragino Kaku Gothic Pro" my-jp-font-size 'medium nil)
+(my-def-font "hiramin" "Monaco" my-ascii-font-size 'medium "Hiragino Mincho Pro" my-jp-font-size 'medium nil)
+(my-def-font "hirasans" "Inconsolata" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light nil)
+(my-def-font "lettergoth" "Letter Gothic Std" my-ascii-font-size 'medium "Hiragino Sans" my-jp-font-size 'light nil)
+(my-def-font "inputmono" "Input Mono Narrow" my-ascii-font-size 'light "Hiragino Sans" my-jp-font-size 'light nil)
+(my-def-font "udkyokasho" "Input Mono Narrow" my-ascii-font-size 'light "UD Digi Kyokasho N-R" my-jp-font-size 'medium nil)
 ;;
 ;;-----------------------------
 ;;  For Linux
 ;;-----------------------------
-(my-def-font "taakoexgoth" "Ubuntu Mono" my-ascii-font-size 'light "TakaoExゴシック" my-jp-font-size 'medium)
-(my-def-font "notosans" "DejaVu Sans Mono" my-ascii-font-size 'light "Noto Sans CJK JP" my-jp-font-size 'light)
-(my-def-font "vlgoth" "DejaVu Sans Mono" my-ascii-font-size 'light "VL Gothic" my-jp-font-size 'light)
+(my-def-font "taakoexgoth" "Ubuntu Mono" my-ascii-font-size 'light "TakaoExゴシック" my-jp-font-size 'medium nil)
+(my-def-font "notosans" "DejaVu Sans Mono" my-ascii-font-size 'light "Noto Sans CJK JP" my-jp-font-size 'light nil)
+(my-def-font "vlgoth" "DejaVu Sans Mono" my-ascii-font-size 'light "VL Gothic" my-jp-font-size 'light nil)
 ;;
 ;;-----------------------------
 ;;  For Windows
 ;;-----------------------------
 ;;
-(my-def-font "meiryo" "Consolas" my-ascii-font-size 'medium "メイリオ" my-jp-font-size 'medium)
-(my-def-font "udkyokashowin" "Consolas" my-ascii-font-size 'medium "UD デジタル 教科書体 N-R" my-jp-font-size 'medium)
+(my-def-font "meiryo" "Consolas" my-ascii-font-size 'medium "メイリオ" my-jp-font-size 'medium nil)
+(my-def-font "udkyokashowin" "Consolas" my-ascii-font-size 'medium "UD デジタル 教科書体 N-R" my-jp-font-size 'medium nil)
 ;;
 ;;-----------------------------
 ;;  General
 ;;-----------------------------
 ;;
-(my-def-font "ricty" "Ricty Diminished" my-ascii-font-size 'medium "Ricty Diminished" my-jp-font-size 'medium)
+(my-def-font "ricty" "Ricty Diminished" my-ascii-font-size 'medium "Ricty Diminished" my-jp-font-size 'medium nil)
 ;;
 ;;===============================================================================================
 ;; Default Font Setup
