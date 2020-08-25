97da44a134702df8d8d48228c53b0fd2b4ecd4c5
hjmr <2620990+hjmr@users.noreply.github.com>
2020-08-25 16:36:55 +0900

    early-init and backup files' location.

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;;-------------------------------
+;; hide menus and tool bars
+;;-------------------------------
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(set-scroll-bar-mode 'nil)
--- init.el
+++ init.el
@@ -146,6 +146,16 @@
 (setq visible-bell nil
       ring-bell-function 'flash-mode-line)
 ;;-------------------------------
+;; backup file
+;;-------------------------------
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist '(("." . "~/.emacs.d/backups"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
+;;-------------------------------
 ;; misc global settings
 ;;-------------------------------
 (setq-default major-mode 'indented-text-mode)
