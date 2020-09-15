66a03f8c04014ab3a1d2221c2a85ff4d2d230758
hjmr <2620990+hjmr@users.noreply.github.com>
2020-09-15 11:10:43 +0900

    move more staff to early-init

--- early-init.el
+++ early-init.el
@@ -1,6 +1,34 @@
 ;;-------------------------------
+;; for in different environment
+;;-------------------------------
+(defmacro exec-if-bound (sexplist)
+  "Execute function in SEXPLIST iff function exists."
+  `(if (fboundp (car ',sexplist))
+       ,sexplist))
+;;
+(defun safe-global-set-key (key command)
+  "Assign KEY to COMMAND iff COMMAND exsits."
+  (when (fboundp command)
+    (global-set-key key command)))
+;;
+(defun safe-define-key (map key command)
+  "Assign KEY in MAP to COMMAND iff COMMAND exsits."
+  (when (and (boundp map)
+             (fboundp command))
+    (define-key (eval map) key command)))
+;;
+(defun font-exists-p (font)
+  "check if FONT exists"
+  (if (null (x-list-fonts font)) nil t))
+;;
+(defun describe-face-at-point ()
+  "Return face used at point."
+  (interactive)
+  (message "%s" (get-char-property (point) 'face)))
+
+;;-------------------------------
 ;; hide menus and tool bars
 ;;-------------------------------
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(set-scroll-bar-mode 'nil)
+(exec-if-bound (menu-bar-mode -1))
+(exec-if-bound (tool-bar-mode -1))
+(exec-if-bound (set-scroll-bar-mode 'nil))
--- init.el
+++ init.el
@@ -43,33 +43,6 @@
       sys-istc-p      (string-match "\\.center\\.kobe-u\\.ac\\.jp$" (system-name))
       )
 ;;-------------------------------
-;; for in different environment
-;;-------------------------------
-(defmacro exec-if-bound (sexplist)
-  "Execute function in SEXPLIST iff function exists."
-  `(if (fboundp (car ',sexplist))
-       ,sexplist))
-;;
-(defun safe-global-set-key (key command)
-  "Assign KEY to COMMAND iff COMMAND exsits."
-  (when (fboundp command)
-    (global-set-key key command)))
-;;
-(defun safe-define-key (map key command)
-  "Assign KEY in MAP to COMMAND iff COMMAND exsits."
-  (when (and (boundp map)
-             (fboundp command))
-    (define-key (eval map) key command)))
-;;
-(defun font-exists-p (font)
-  "check if FONT exists"
-  (if (null (x-list-fonts font)) nil t))
-;;
-(defun describe-face-at-point ()
-  "Return face used at point."
-  (interactive)
-  (message "%s" (get-char-property (point) 'face)))
-;;-------------------------------
 ;;  Package System
 ;;-------------------------------
 (when (require 'package nil t)
@@ -131,14 +104,6 @@
 ;; Japanese environment
 ;;-------------------------------
 (set-language-environment 'Japanese)
-;;-------------------------------
-;; hide menus and tool bars
-;;-------------------------------
-;; it has been done in early-init.el in Emacs 27
-(when (< emacs-major-version 27)
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (set-scroll-bar-mode 'nil))
 ;;-------------------------------
 ;; visible-bell
 ;;-------------------------------
